36628ab1c23289613affa0340718ea6a71ac9b94
Filip Crijanovschi <filipcrijanovschi@gmail.com>
2023-12-06 18:26:42 +0200

    Some implemintations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import {SuccessAlertComponent} from "./success-alert/success-alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
